9a606ae6d154d706a5634f762d5643861a999a9a
Milan Vit <milanvit@milanvit.net>
2018-10-03 18:39:10 +0900

    Added secure password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
   validates :email, presence: true, length: {maximum: 105}, uniqueness: {case_sensitive: false},
             format: {with: URI::MailTo::EMAIL_REGEXP}
+  has_secure_password
 end
