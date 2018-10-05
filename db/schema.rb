9da1a4888763ce7be29572661290ec39ace66df8
Milan Vit <milanvit@milanvit.net>
2018-10-05 11:14:44 +0900

    Added article and category association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_081213) do
+ActiveRecord::Schema.define(version: 2018_10_05_020454) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
