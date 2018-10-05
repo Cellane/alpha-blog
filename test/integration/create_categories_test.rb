8c053b5e0b146464ca5809cd63f40492f0836ff1
Milan Vit <milanvit@milanvit.net>
2018-10-05 10:33:08 +0900

    Fixed tests and updated navigation bar

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: "Cellane", email: "milanvit@milanvit.net", password: "nbusr123", admin: true)
+  end
+
   test "get new category form and create category" do
+    sign_in_as(@user, "nbusr123")
     get new_category_path
     assert_template "categories/new"
     assert_difference "Category.count", 1 do
@@ -12,8 +17,9 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template "categories/index"
     assert_match "sports", response.body
   end
-  
+
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "nbusr123")
     get new_category_path
     assert_template "categories/new"
     assert_no_difference "Category.count" do
